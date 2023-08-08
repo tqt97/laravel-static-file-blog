f236b62567d0c980b999d8bdd85dd59a022ac102
TuanDev <kutuanonline199@gmail.com>
2023-08-08 14:22:01 +0700

    install tailwincss and init template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h1>List Blog here</h1>
+</x-app-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans]
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+}
